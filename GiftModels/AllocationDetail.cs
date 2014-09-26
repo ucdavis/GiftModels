ec0ea65942d30b71a946caf1ed5c5e7f2cd926c6
Scott Kirkland <srkirkland@gmail.com>
2014-09-26 10:02:32 -0700

    new allocation detail info

--- GiftModels/AllocationDetail.cs
+++ GiftModels/AllocationDetail.cs
@@ -4,6 +4,11 @@
     {
         public string Allocation_Code { get; set; }
         public string Account { get; set; }
+        public string Long_Name { get; set; }
         public string DisplayName { get; set; }
+        public string Fund_Name { get; set; }
+        public string Alloc_Dept_Code { get; set; }
+        public string Alloc_School { get; set; }
+        public string Agency { get; set; }
     }
 }
