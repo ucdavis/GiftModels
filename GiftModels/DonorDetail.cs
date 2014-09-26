3e4c6d972f842d2f9a52b63d3d5409f6fe0701af
Scott Kirkland <srkirkland@gmail.com>
2014-09-26 09:58:03 -0700

    donor info

--- GiftModels/DonorDetail.cs
+++ GiftModels/DonorDetail.cs
@@ -3,6 +3,20 @@
     public class DonorDetail
     {
         public string Id_Number { get; set; }
+        public string First_Name { get; set; }
+        public string Last_Name { get; set; }
+        public string FullName { get; set; }
+        public string Jnt_Gifts_Ind { get; set; }
+        public string IsJoint { get; set; }
+        public string ReportName { get; set; }
+        public string SpouseName { get; set; }
         public string DisplayName { get; set; }
+        public string Email { get; set; }
+        public string EmailHash { get; set; }
+        public string Street1 { get; set; }
+        public string City { get; set; }
+        public string State_Code { get; set; }
+        public string Zipcode { get; set; }
+        public string Address { get; set; }
     }
 }
