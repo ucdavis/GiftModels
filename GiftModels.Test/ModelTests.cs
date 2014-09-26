f0a711da7b47d359d00717e92bfa731243e4acd7
Scott Kirkland <srkirkland@gmail.com>
2014-09-26 09:42:00 -0700

    new there's a test suite!

---
+++ GiftModels.Test/ModelTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+
+namespace GiftModels.Test
+{
+    [TestClass]
+    public class ModelTests
+    {
+        [TestMethod]
+        public void Deserialize()
+        {
+            var content = File.ReadAllText("GiftExample.json");
+
+            var giftdetail = JsonConvert.DeserializeObject<GiftDetails>(content);
+
+            Assert.IsNotNull(giftdetail);
+            Assert.AreEqual("123456789", giftdetail.PrimaryDonor.Detail.Id_Number);
+        }
+    }
+}
