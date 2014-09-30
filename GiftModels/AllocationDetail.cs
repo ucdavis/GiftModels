10b3ecdc3b293bbf3ff8c4c5dddb62b9a8a9c3a7
John Knoll <jpknoll@ucdavis.edu>
2014-09-30 15:06:20 -0700

    get method

--- GiftModels/AllocationDetail.cs
+++ GiftModels/AllocationDetail.cs
@@ -5,7 +5,13 @@
         public string AllocationCode { get; set; }
         public string Account { get; set; }
         public string LongName { get; set; }
-        public string DisplayName { get; set; }
+        public string DisplayName
+        {
+            get
+            {
+                return string.Format("{0}/{1} ({2})", AllocationCode, Account, LongName);
+            }
+        }
         public string FundName { get; set; }
         public string DeptartmentCode { get; set; }
         public string SchoolCode { get; set; }
@@ -15,5 +21,6 @@
         /// R: Regents
         /// </summary>
         public string Agency { get; set; }
+        public string Purpose { get; set; }
     }
 }
