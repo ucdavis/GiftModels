894d04bad6852ddb603972d588f6e77154677f54
John Knoll <jpknoll@ucdavis.edu>
2014-10-14 10:36:20 -0700

    New property

--- GiftModels/DonorDetail.cs
+++ GiftModels/DonorDetail.cs
@@ -9,6 +9,7 @@ namespace GiftModels
         /// Advance Entity Id
         /// </summary>
         public string IdNumber { get; set; }
+        public string Prefix { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string ReportName { get; set; }
