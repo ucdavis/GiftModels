7f33d8237bdc4b49f8f5b5f6366c9210cf4f207e
John Knoll <jpknoll@ucdavis.edu>
2015-05-13 12:07:19 -0700

    type to content type and file type

--- GiftModels/Attachment.cs
+++ GiftModels/Attachment.cs
@@ -4,10 +4,32 @@ namespace GiftModels
     public class Attachment
     {
         public string Name { get; set; }
-        public string Type { get; set; }
+
+        /// <summary>
+        /// html file type (json, application, image, w/e)
+        /// </summary>
+        public string ContentType { get; set; }
+
+        /// <summary>
+        /// purpose (check, coupon, tsp cover sheet, w/e)
+        /// </summary>
+        public string FileType { get; set; }
+
         public string Uri { get; set; }
+
+        /// <summary>
+        /// azure identifier
+        /// </summary>
         public string Identifier { get; set; }
+
+        /// <summary>
+        /// bytes
+        /// </summary>
         public string Size { get; set; }
+
+        /// <summary>
+        /// catch all notes, description, w/e
+        /// </summary>
         public string Description { get; set; }
     }
 }
