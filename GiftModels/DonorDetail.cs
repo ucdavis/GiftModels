6eb48cb37cfc016469c8572ebd5d34429b8fc540
John Knoll <jpknoll@ucdavis.edu>
2015-05-14 13:22:18 -0700

    2nd street, same as the first
    little bit better, little bit worse

--- GiftModels/DonorDetail.cs
+++ GiftModels/DonorDetail.cs
@@ -87,6 +87,7 @@ namespace GiftModels
         }
         public string Phone { get; set; }
         public string Street1 { get; set; }
+        public string Street2 { get; set; }
         public string City { get; set; }
         public string State { get; set; }
         public string Zip { get; set; }
