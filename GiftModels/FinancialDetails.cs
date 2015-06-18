3b44d7c4cc786ad0b2ce3c908c222c6ab90fd8c6
Ken Taylor <kentaylor@ucdavis.edu>
2015-06-18 11:26:44 -0700

    Revised according to query.

--- GiftModels/FinancialDetails.cs
+++ GiftModels/FinancialDetails.cs
@@ -8,44 +8,83 @@ namespace GiftModels
 {
     public class FinancialDetails
     {
-        public string AdvanceReceiptNumber { get; set; }
         public virtual string KfsKey { get; set; }
+
+        public string AdvanceReceiptNumber { get; set; }
+
         public virtual string PrimaryDonor { get; set; }
-        public virtual decimal Total { get; set; }
+
+        public virtual decimal TotalAmount { get; set; }
+
         public virtual decimal TotalPremiumAmount { get; set; }
-        public virtual string Prem { get; set; } //Yes/No
-        public virtual decimal PremiumAdjustedAmount {
-            get { return 0.0m; }//TODO: Do the calculation
-        }
-        public virtual string AllocationNumber { get; set; }
+
+        public virtual string Prem { get; set; } // Y/N
+
+        public virtual decimal PremiumAdjustedGiftAmount { get; set; }
+
+        public virtual string AllocationCode { get; set; }
+
         public virtual string AllocationName { get; set; }
+
         public virtual string Agency { get; set; }
+
         public virtual string FundType { get; set; }
-        public virtual string FisAccountNumber { get; set; }
+
+        public virtual string FisAccount { get; set; }
+
         public virtual string FisSubAccount { get; set; }
+
         public virtual string GiftFeeCode { get; set; }
+
         public virtual string GiftFeeExceptionName { get; set; }
-        public virtual string AfFee { get; set; }
+
+        public virtual string AfFee { get; set; } // Y/N
+
         public virtual string BenefitingUnitCode { get; set; }
+
         public virtual string BenefitingUnitName { get; set; }
-        public virtual DateTime JvBaDate { get; set; }
-        public virtual string JvBaScrubberNumber { get; set; }
-        //public virtual string Comments { get; set; } //Great?
+
+        //public virtual DateTime JvBaDate { get; set; } //Great
+
+        //public virtual string JvBaScrubberNumber { get; set; } //Great
+
+        //public virtual string Comments { get; set; } //Great
+
         //public virtual string Status { get; set; } //Great
-        public virtual decimal GiftBalance { get; set; }
+
+        //public virtual decimal GiftBalance { get; set; } //Great
+
         public virtual string UcFundChart { get; set; }
+
         public virtual string UcFundNumber { get; set; }
-        public virtual string ScrubberChart { get; set; } //Great?
-        public virtual string ScrubberAccount { get; set; } //Great?
+
+        public virtual string ScrubberChart { get; set; }
+
+        public virtual string ScrubberAccount { get; set; }
+
         public virtual string RevenueChart { get; set; }
+
         public virtual string RevenueAccount { get; set; }
+
         public virtual string PremiumChart { get; set; }
+
         public virtual string PremiumAccount { get; set; }
+
+        public virtual string PremiumAmount { get; set; }
+
         public virtual string ExpenseChart { get; set; }
+
         public virtual string ExpenseAccount { get; set; }
+
         public virtual string ExpenseSubAccount { get; set; }
+
         public virtual string GiftFeeChart { get; set; }
+
         public virtual string GiftFeeAccount { get; set; }
+
+        public virtual DateTime CreationDate { get; set; } //Great?
+
+        public virtual DateTime UpdateDate { get; set; } // Great?
 
 
 
