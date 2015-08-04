205b959a70f3e054b3de1a238577fb1e7abe27c4
Ken Taylor <kentaylor@ucdavis.edu>
2015-08-04 09:24:48 -0700

    This should help check for n elements in a collection.

---
+++ GiftModels/Helpers/DataAnnotationsHelper.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.ComponentModel.DataAnnotations;
+
+namespace GiftModels.Helpers
+{
+    /// <summary>
+    /// Class to extend DataAnnotations to handle additional validation.
+    /// </summary>
+    public class DataAnnotationsHelper
+    {
+        /// <summary>
+        /// Data Validation Methods to test Collections.
+        /// </summary>
+        public class MinListLength : ValidationAttribute
+        {
+            private readonly int _length;
+
+            /// <summary>
+            /// Test if a collection has at least "n" elements 
+            /// </summary>
+            /// <param name="length"></param>
+            public MinListLength(int length)
+                : base("{0} Must have at least " + length + " element(s).")
+            {
+                _length = length;
+            }
+
+            protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+            {
+                var collection = value as ICollection;
+
+                if (collection == null || collection.Count < _length)
+                {
+                    var errorMessage = FormatErrorMessage(validationContext.DisplayName);
+                    return new ValidationResult(errorMessage);
+                }
+
+                return ValidationResult.Success;
+            }
+        }
+    }
+}
