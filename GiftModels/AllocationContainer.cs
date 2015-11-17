b55e6f2d5efaa6e30a6ca4dfdb6c68127ef5d0a0
John Knoll <jpknoll@ucdavis.edu>
2015-11-16 16:18:35 -0800

    move pledge info to allocation

--- GiftModels/AllocationContainer.cs
+++ GiftModels/AllocationContainer.cs
@@ -4,6 +4,13 @@
     {
         public decimal Amount { get; set; }
 
+        public string PledgeNumber { get; set; }
+
+        /// <summary>
+        /// Valid Values: Null/Empty, Existing, New, NewWithPayment
+        /// </summary>
+        public string PledgeType { get; set; }
+
         public AllocationDetail Detail { get; set; }
     }
 }
