c0e50e2c0c28658ae092a3c99782041dbf91c02a
Scott Kirkland <srkirkland@gmail.com>
2016-01-20 10:15:23 -0800

    include actor info

--- GiftModels/Rejection.cs
+++ GiftModels/Rejection.cs
@@ -2,10 +2,16 @@
 
 namespace GiftModels
 {
+    /// <summary>
+    /// Records a rejection along with the person who rejected this gift
+    /// </summary>
     public class Rejection
     {
         public string Code { get; set; }
         public string Description { get; set; }
+        public string Actor { get; set; }
+        public string ActorName { get; set; }
         public DateTime CreatedDate { get; set; }
+
     }
 }
