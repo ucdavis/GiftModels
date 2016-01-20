5cb7a3f6673fdac2896c6ba0c42258fc8cbb15d9
Scott Kirkland <srkirkland@gmail.com>
2016-01-20 10:11:09 -0800

    new rejection object, part of review details

---
+++ GiftModels/Rejection.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace GiftModels
+{
+    public class Rejection
+    {
+        public string Code { get; set; }
+        public string Description { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+}
