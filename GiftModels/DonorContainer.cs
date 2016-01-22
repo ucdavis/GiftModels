84dfc0af7cb97a88f1de682d2146f6037aeea5bb
John Knoll <jpknoll@ucdavis.edu>
2016-01-21 16:56:19 -0800

    comments

--- GiftModels/DonorContainer.cs
+++ GiftModels/DonorContainer.cs
@@ -2,6 +2,9 @@ namespace GiftModels
 {
     public class DonorContainer
     {
+        /// <summary>
+        /// Differentiate associates, pets, emtpy acknowledgements, etc
+        /// </summary>
         public string DonorType { get; set; }
 
         public DonorDetail Detail { get; set; }
