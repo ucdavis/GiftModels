30715c6b5cffd6cbbb3fd6fd4c19a881e46c3941
Stephen D. High <shigh@ucdavis.edu>
2016-01-25 09:00:34 -0800

    refactored acknowledgement to list of acknowledgements

--- GiftModels/DonorContainer.cs
+++ GiftModels/DonorContainer.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+
 namespace GiftModels
 {
     public class DonorContainer
@@ -11,7 +13,7 @@ namespace GiftModels
 
         public Affiliate Affiliate { get; set; }
 
-        public Acknowledgement Acknowledgement { get; set; }
+        public List<Acknowledgement> Acknowledgements { get; set; }
 
         public decimal Amount { get; set; }
 
