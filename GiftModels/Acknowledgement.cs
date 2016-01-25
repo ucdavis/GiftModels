9f1652018fb5a64c4ae29587d2233757199d60fe
Stephen D. High <shigh@ucdavis.edu>
2016-01-25 09:02:18 -0800

    acknowledgement property changes
    split address into 2 properties
    added email
    added giving code number

--- GiftModels/Acknowledgement.cs
+++ GiftModels/Acknowledgement.cs
@@ -14,6 +14,21 @@ namespace GiftModels
 
         public string Name { get; set; }
 
-        public string Address { get; set; }
+        /// <summary>
+        /// Street address including suite/apt number
+        /// </summary>
+        public string Address1 { get; set; }
+
+        /// <summary>
+        /// City, State, Zip
+        /// </summary>
+        public string Address2 { get; set; }
+        
+        public string Email { get; set; }
+
+        /// <summary>
+        /// Pet acknowledgements require a giving code number for reference
+        /// </summary>
+        public string GivingCodeNumber { get; set; }
     }
 }
