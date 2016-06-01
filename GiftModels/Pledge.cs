e53d460e7d0da5f447a7cd773a6fed0846eab44f
John Knoll <jpknoll@ucdavis.edu>
2016-06-01 15:20:39 -0700

    new fields and comments

--- GiftModels/Pledge.cs
+++ GiftModels/Pledge.cs
@@ -5,12 +5,28 @@ namespace GiftModels
     public class Pledge
     {
         public string PledgeNumber { get; set; }
+
         public string PledgeType { get; set; }
 
+        /// <summary>
+        /// Total Amount Pledged
+        /// </summary>
         public decimal Amount { get; set; }
 
         public string Frequency { get; set; }
+
         public string FrequencyDetails { get; set; }
+
         public DateTime? InitialPayDate { get; set; }
+
+        /// <summary>
+        /// Amount to be paid per frequency period
+        /// </summary>
+        public decimal PeriodAmount { get; set; }
+
+        /// <summary>
+        /// Total Count of frequency periods
+        /// </summary>
+        public int PeriodCount { get; set; }
     }
 }
