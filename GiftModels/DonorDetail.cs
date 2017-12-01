489058c65f1f9dcb8c770a95a5dba8742c4bb765
Stephen D. High <stephen@complia.net>
2017-12-01 14:04:28 -0800

    refactor address to string

--- GiftModels/DonorDetail.cs
+++ GiftModels/DonorDetail.cs
@@ -86,28 +86,42 @@ namespace GiftModels
         {
             get
             {
-                var result = "";
+                //fast return if no address
+                if (string.IsNullOrWhiteSpace(Street1)) return string.Empty;
+
+                //build address
+                var sb = new StringBuilder();
+
                 if (!string.IsNullOrWhiteSpace(Street1))
                 {
-                    result += Street1 + ", ";
+                    sb.Append(Street1);
+                }
+                if (!string.IsNullOrWhiteSpace(Street2))
+                {
+                    sb.Append(" ")
+                      .Append(Street2);
                 }
                 if (!string.IsNullOrWhiteSpace(City))
                 {
-                    result += City + ", ";
+                    sb.Append(", ")
+                      .Append(City);
                 }
                 if (!string.IsNullOrWhiteSpace(State))
                 {
-                    result += State + " ";
+                    sb.Append(" ")
+                      .Append(State);
                 }
                 if (!string.IsNullOrWhiteSpace(Zip))
                 {
-                    result += Zip;
+                    sb.Append(" ")
+                      .Append(Zip);
                 }
                 if (!string.IsNullOrWhiteSpace(Country))
                 {
-                    result += " " + Country;
+                    sb.Append(" ")
+                      .Append(Country);
                 }
-                return result.Trim(' ', ',');
+                return sb.ToString();
             }
         }
 
